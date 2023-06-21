53760c194ee8b9c1d145791dce48f7865a22843e
3p4r4 <mendoza124302@gmail.com>
2023-06-20 22:25:29 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <section className="side-bar">
+        <button id="new-chat">+ Nuevo Chat</button>
+        <ul className="history">
+          <li>tester</li>
+        </ul>
+        <nav>
+          <p>Hecho por Lenin Mendoza</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>Chat-GPT Mendozalz</h1>
+        <ul className="fedd">
+          <li>texto</li>
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <button id="submit">➢</button>
+          </div>
+          <p className="info">Chat GPT en REACT - Version 1.0</p>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
